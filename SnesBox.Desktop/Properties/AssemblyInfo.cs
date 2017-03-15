03e6e8b59803c6d20be550f608de8272c870b8af
Justin Bozalina <justin@bozalina.com>
2017-03-15 13:07:11 -0400

    Adding a desktop app

--- SnesBox.Desktop/Properties/AssemblyInfo.cs
+++ SnesBox.Desktop/Properties/AssemblyInfo.cs
@@ -5,24 +5,20 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SnesBox")]
-[assembly: AssemblyProduct("SnesBox")]
+[assembly: AssemblyTitle("SnesBox.Desktop")]
+[assembly: AssemblyProduct("SnesBox.Desktop")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("88e4baf9-ee2b-44a1-a2e9-a31f17827472")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bb71bc3b-dcbd-4ef1-ab1b-dc5e17fcb63b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,4 +27,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
